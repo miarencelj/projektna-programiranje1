4d0327320396c96f908243fb675b6463e332bc1c
Docker <docker@example.com>
2024-07-23 14:14:39 +0100

    popravek

--- src/definicije/avtomat.mli
+++ src/definicije/avtomat.mli
@@ -5,9 +5,9 @@ val dodaj_stanje : Stanje.t -> t -> t
 val dodaj_prehod : Stanje.t -> char -> Stanje.t -> t -> t
 val dodaj_izhod : Stanje.t -> string -> t -> t
 val prehodna_funkcija : t -> Stanje.t -> char -> Stanje.t option
-val izhodna_funkcija : t -> Stanje.t -> string
+val izhodna_funkcija : t -> Stanje.t -> string option
 val zacetno_stanje : t -> Stanje.t
 val seznam_stanj : t -> Stanje.t list
 val seznam_prehodov : t -> (Stanje.t * char * Stanje.t) list
 val cestnina : t
-val preberi_niz : t -> Stanje.t -> string -> Stanje.t * string list
+val preberi_niz : t -> Stanje.t -> string -> Stanje.t * string option list
